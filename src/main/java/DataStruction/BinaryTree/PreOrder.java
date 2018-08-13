5a01cf03c616e7465c8143005882f7e76d307062
tianbo <tianbo@staff.weibo.com>
2018-08-13 16:11:59 +0800

    first commit

---
+++ src/main/java/DataStruction/BinaryTree/PreOrder.java
@@ -0,0 +1,7 @@
+package DataStruction.BinaryTree;
+
+/**
+ * Created by tianbo on 2018/8/13.
+ */
+public class PreOrder {
+}
---
+++ src/main/java/DataStruction/test.java
@@ -0,0 +1,7 @@
+package DataStruction;
+
+/**
+ * Created by tianbo on 2018/8/13.
+ */
+public class test {
+}
